2e7cdba9b4199edcad84f208bde78e7306ceaf16
vanraj0007 <vchudasma1997@gmail.com>
2022-01-06 14:22:06 +0530

    scj

--- audio_player/seriealizers.py
+++ audio_player/seriealizers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from audio_player.models import Audio
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-# heloo
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
